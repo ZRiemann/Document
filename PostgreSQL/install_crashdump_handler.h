5c49aca751bac51ace93a5dd3ff512ce3bd9e660
ZRiemann <25452483@qq.com>
2016-07-20 19:30:36 +0800

    apue: c6 system file and infomation; PostgreSQL: smgr

---
+++ PostgreSQL/install_crashdump_handler.h
@@ -0,0 +1,14 @@
+#ifndef _INSTALL_CRASHDUMP_HANDLER_
+#define _INSTALL_CRASHDUMP_HANDLER_
+#ifdef __cplusplus
+extern "C"{
+#endif
+/**@file install_crashdump_handler.h
+ * @biref install crash dump handler
+ */
+void install_crashdump_handler(void);
+
+#ifdef __cplusplus
+}
+#endif
+#endif
---
+++ apue/c6_2getpwname.c
@@ -0,0 +1,8 @@
+/**@file c6_2getpwnam
+ * @biref list all user names
+ */
+#include <stdio.h>
+#include <pwd.h>
+#include <stddef.h>
+#include <string.h>
+	
