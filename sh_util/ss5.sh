b46d0600f5e2e794eb2b00009cbe6054e3e7a8a4
Z.Riemann <25452483@qq.com>
2020-06-10 16:10:27 +0800

    [update] ss5.sh

--- sh_util/ss5.sh
+++ sh_util/ss5.sh
@@ -2,14 +2,15 @@
 
 version=ss5-3.8.9
 yum -y install gcc gcc-c++ automake make pam-devel openldap-devel cyrus-sasl-devel openssl-devel
+cd /tmp
 wget https://nchc.dl.sourceforge.net/project/ss5/ss5/3.8.9-8/ss5-3.8.9-8.tar.gz
 tar zxvf ./${version}.tar.gz
 cd ${version}
 ./configure
 make
 make install
-
 chmod +x /etc/init.d/ss5
+cd -
 
 echo "
 vi /etc/sysconfig/ss5  
