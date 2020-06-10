92d0daa3e6b0efc744d8d2a9446623e33621be9b
Z.Riemann <25452483@qq.com>
2020-06-10 16:06:40 +0800

    [update] ss5.sh

--- sh_util/ss5.sh
+++ sh_util/ss5.sh
@@ -11,8 +11,18 @@ make install
 
 chmod +x /etc/init.d/ss5
 
-echo "modify /etc/sysconf/ss5/conf"
-echo "auth 0.0.0.0/0 - -"
-echo "permit - 0.0.0.0/0 - - - - -"
-echo "systemctl restart ss5"
-echo "systemctl status ss5"
+echo "
+vi /etc/sysconfig/ss5  
+SS5_OPTS=" -u root -b 0.0.0.0:18080" #绑定端口为18080
+systemctl start ss5 #启动服务
+
+#增加用户登录权限
+vi /etc/opt/ss5/ss5.conf  
+auth 0.0.0.0/0 - u  
+permit u 0.0.0.0/0 - 0.0.0.0/0 - - - - -
+
+#设置用户名和密码，一个用户和密码一行，用空格间隔
+vi /etc/opt/ss5/ss5.passwd  
+user1 123456
+user2 123456
+"
