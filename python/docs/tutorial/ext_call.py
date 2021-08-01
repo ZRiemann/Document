7375a5a71c25146634c9d7e6564bc3a565d8f76d
z.riemann <25452483@qq.com>
2021-08-01 21:50:16 +0800

    Add python anaconda3.org

--- python/docs/tutorial/ext_call.py
+++ python/docs/tutorial/ext_call.py
@@ -1,9 +1,9 @@
-from time import time,ctime
+#!/usr/bin/python3.8
 
-def ext_add(a, b):
+def ext_add (a, b):
         return a + b
 
-def multiply(a,b):
+def multiply (a,b):
     print("Will compute", a, "times", b)
     c = 0
     for i in range(0, a):
@@ -11,4 +11,4 @@ def multiply(a,b):
     return c
 
 if __name__ == '__main__':
-        print('Today is', ctime(time()), ext_add(1, 1), multiply(3,4))
+        print(ext_add(1, 1), multiply(3,4))
